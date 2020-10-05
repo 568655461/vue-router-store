fef3151b56288e9c30659cd12e6158f0fdfbee11
senlyn <568655461@qq.com>
2020-10-05 23:11:54 +0800

    getters方法实现

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,7 @@
     <h1>{{ msg }}</h1>
     <p @click="$store.commit('add')">counter:{{$store.state.counter}}</p>
     <p @click="$store.dispatch('add')">async counter: {{$store.state.counter}}</p>
+    <p>double counter:{{$store.getters.doubleCounter}}</p>
     <!-- 组件通信 -->
     <!-- <Communication></Communication> -->
     <!-- 表单 -->
--- src/kstore/kvuex.js
+++ src/kstore/kvuex.js
@@ -1,24 +1,47 @@
 //1.插件，实现挂在$store
 //2.实现store
 
+
 let Vue;
 
 class Store{
     constructor(options){
+        
+        this._mutations = options.mutations;
+        this._actions = options.actions;
+        this._wrapperGetters = options.getters;
+        this.commit = this.commit.bind(this);//绑定this
+        this.dispatch = this.dispatch.bind(this);
+        //定义computed选项
+        const computed = {};
+        const store = this;//绑定this
+        this.getters = {};//暴露给用户
+
+        Object.keys(this._wrapperGetters).forEach((key) => {
+            const fn = store._wrapperGetters[key];
+            computed[key] = function(){//computed方法无参数，需要包装一层,计算结果存到了_vm中computed中
+                return fn(store.state)
+            }
+
+            Object.defineProperty(store.getters,key,{
+                get:() => {//只有只读属性
+                    return store._vm[key];
+                }
+            })
+        })
+
+        
+
         // this.state = new Vue({
         //     data:options.state//借鸡生蛋，把数据变成响应式,实际上不会这么写，包装一下
         // })
         this._vm = new Vue({
             data:{
                 $$state:options.state//不代理，私有
-            }
+            },
+            computed//运用了计算属性，缓存计算结果
         })
-        this._mutations = options.mutations;
-        this._actions = options.actions;
-        this.commit = this.commit.bind(this);//绑定this
-        this.dispatch = this.dispatch.bind(this);
 
-        // this.getters = {}
     }
     
     get state(){
@@ -48,7 +71,6 @@ class Store{
         entry(this,payload)//传入对象
     }
 
-    
 }
 
 function install(_Vue){
